9d0de8ad2161fbd2730284b7897d55e48c09ac8a
eyobbaye <eyob.bay.com>
2025-02-28 13:55:24 +0300

    script add

---
+++ scrept.js
@@ -0,0 +1,47 @@
+// Salary calculate
+let taxRate = 0;
+let deduction = 0;
+let salaryIncomeTax = 0;
+let companyPensiton;
+let grosSalary = Number(prompt("Enter your salary"));
+if (grosSalary <= 600) {
+  let netIncome = grosSalary;
+  taxRate = 0;
+  let employeePensiton = 0;
+  salaryIncomeTax = 0;
+  companyPensiton = grosSalary * 0.11;
+  deduction = 0;
+  console.log(
+    `taxRate: ${taxRate}, deduction: ${deduction}, salaryIncomeTax: ${salaryIncomeTax}, employeePensiton: ${employeePensiton}, companyPenstion: ${companyPensiton}, netIncome: ${netIncome}`
+  );
+  console.log(`NON taxable: Your salary is ${netIncome}`);
+} else if (grosSalary >= 601 || grosSalary <= 1650) {
+  taxRate = grosSalary * 0.1;
+  deduction = 60;
+} else if (grosSalary === 1651 || grosSalary <= 3200) {
+  taxRate = grosSalary * 0.15;
+  deduction = 142.5;
+} else if (grosSalary === 3201 || grosSalary <= 5200) {
+  taxRate = grosSalary * 0.2;
+  deduction = 302.5;
+} else if (grosSalary === 5201 || grosSalary <= 7800) {
+  taxRate = grosSalary * 0.25;
+  deduction = 565;
+} else if (grosSalary === 7801 || grosSalary <= 10900) {
+  taxRate = grosSalary * 0.3;
+  deduction = 955;
+} else if (grosSalary >= 10901) {
+  taxRate = grosSalary * 0.35;
+  deduction = 1500;
+  employeePensiton = 0;
+  console.log(taxRate, deduction);
+}
+salaryIncomeTax = Math.floor(taxRate - deduction);
+employeePensiton = Math.floor(grosSalary * 0.07);
+companyPensiton = Math.floor(grosSalary * 0.11);
+netIncome = grosSalary - salaryIncomeTax - employeePensiton;
+if (grosSalary >= 600) {
+  console.log(
+    `taxRate: ${taxRate}, deduction: ${deduction}, salaryIncomeTax: ${salaryIncomeTax}, employeePensiton: ${employeePensiton}, companyPenstion: ${companyPensiton}, netIncome: ${netIncome}`
+  );
+}
